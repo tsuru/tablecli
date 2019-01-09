495c987cef629a834a00ee2f12a97fceea4e8dd1
Cezar Sa Espinola <cezarsa@gmail.com>
2019-01-09 12:31:17 -0200

    performance improvements when rendering tables
    $ benchstat old.txt new.txt
    name                old time/op    new time/op    delta
    String-4              1.34ms ± 5%    0.46ms ± 1%  -65.58%  (p=0.001 n=6+7)
    StringWithColor-4     1.62ms ± 2%    0.87ms ± 3%  -46.55%  (p=0.001 n=7+7)
    StringWithResize-4    4.31ms ± 1%    1.41ms ± 1%  -67.19%  (p=0.001 n=6+7)
    
    name                old alloc/op   new alloc/op   delta
    String-4               463kB ± 0%     253kB ± 0%  -45.31%  (p=0.001 n=7+7)
    StringWithColor-4      482kB ± 0%     384kB ± 0%  -20.25%  (p=0.000 n=6+7)
    StringWithResize-4    1.19MB ± 0%    0.74MB ± 0%  -37.88%  (p=0.008 n=6+7)
    
    name                old allocs/op  new allocs/op  delta
    String-4               15.3k ± 0%      5.0k ± 0%  -66.97%  (p=0.001 n=7+7)
    StringWithColor-4      15.7k ± 0%      6.2k ± 0%  -60.14%  (p=0.001 n=7+7)
    StringWithResize-4     51.6k ± 0%     15.6k ± 0%  -69.86%  (p=0.001 n=7+7)

--- render.go
+++ render.go
@@ -12,22 +12,27 @@ import (
 	"sort"
 	"strings"
 	"unicode"
+	"unicode/utf8"
 
 	"golang.org/x/crypto/ssh/terminal"
 )
 
 var TableConfig = struct {
-	BreakOnAny bool
-	ForceWrap  bool
+	BreakOnAny  bool
+	ForceWrap   bool
+	MaxTTYWidth int
 }{
-	BreakOnAny: false,
-	ForceWrap:  false,
+	BreakOnAny:  false,
+	ForceWrap:   false,
+	MaxTTYWidth: 0,
 }
 
 var ignoredPatterns = []*regexp.Regexp{
 	regexp.MustCompile("\033\\[\\d+;\\d+;\\d+m"),
 	regexp.MustCompile("\033\\[0m"),
 }
+
+var ignoredPattern = regexp.MustCompile("\033\\[\\d+;\\d+;\\d+m|\033\\[0m")
 
 type Table struct {
 	Headers       Row
@@ -54,7 +59,7 @@ func (t *Table) SortByColumn(columns ...int) {
 	sort.Sort(rowSliceByColumn{rowSlice: t.rows, columns: columns})
 }
 
-func (t *Table) addRows(rows rowSlice, sizes []int, buf *bytes.Buffer) {
+func (t *Table) addRows(rows rowSlice, sizes []int, buf *strings.Builder) {
 	for _, row := range rows {
 		extraRows := rowSlice{}
 		for column, field := range row {
@@ -216,8 +221,11 @@ func (t *Table) String() string {
 	if terminal.IsTerminal(terminalFd) {
 		ttyWidth, _, _ = terminal.GetSize(terminalFd)
 	}
+	if TableConfig.MaxTTYWidth > 0 && (ttyWidth == 0 || ttyWidth > TableConfig.MaxTTYWidth) {
+		ttyWidth = TableConfig.MaxTTYWidth
+	}
 	sizes := t.resizeLargestColumn(ttyWidth)
-	buf := bytes.NewBuffer(nil)
+	buf := &strings.Builder{}
 	t.separator(buf, sizes)
 	if t.Headers != nil {
 		for column, header := range t.Headers {
@@ -248,10 +256,20 @@ func (t *Table) Rows() int {
 }
 
 func runeLen(s string) int {
-	for _, p := range ignoredPatterns {
-		s = p.ReplaceAllString(s, "")
+	if strings.IndexByte(s, '\033') == -1 {
+		return utf8.RuneCountInString(s)
 	}
-	return len([]rune(s))
+	positions := ignoredPattern.FindAllStringIndex(s, -1)
+	if len(positions) == 0 {
+		return utf8.RuneCountInString(s)
+	}
+	var count int
+	start := 0
+	for _, pos := range positions {
+		count += utf8.RuneCountInString(s[start:pos[0]])
+		start = pos[1]
+	}
+	return count
 }
 
 func (t *Table) columnsSize() []int {
@@ -284,7 +302,7 @@ func (t *Table) columnsSize() []int {
 	return sizes
 }
 
-func (t *Table) separator(buf *bytes.Buffer, sizes []int) {
+func (t *Table) separator(buf *strings.Builder, sizes []int) {
 	for _, sz := range sizes {
 		buf.WriteString("+")
 		buf.Write(bytes.Repeat([]byte("-"), sz+2))
